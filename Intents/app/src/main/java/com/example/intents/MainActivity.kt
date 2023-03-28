b00ab0b4e368a221fa23ec2865a3ca13ee3fbcf4
Ashfaque <shaikhashfaque220@gmail.com>
2023-03-28 13:30:20 +0530

    Webview

--- Intents/app/src/main/java/com/example/intents/MainActivity.kt
+++ Intents/app/src/main/java/com/example/intents/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.intents
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +13,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btnnextAct = findViewById<Button>(R.id.button)
+        val cardCam = findViewById<CardView>(R.id.CardCam)
+
+        cardCam.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(intent)
+        }
 
         btnnextAct.setOnClickListener {
             // open a new activity
--- Intents/app/src/main/java/com/example/intents/MainActivity2.kt
+++ Intents/app/src/main/java/com/example/intents/MainActivity2.kt
@@ -1,9 +1,11 @@
 package com.example.intents
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.cardview.widget.CardView
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +13,15 @@ class MainActivity2 : AppCompatActivity() {
         setContentView(R.layout.activity_main2)
 
         val btnpreAct = findViewById<Button>(R.id.button2)
+
+        val cardGoogle = findViewById<CardView>(R.id.Cardgoogle)
+
+        cardGoogle.setOnClickListener {
+
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://www.google.com/")
+            startActivity(intent)
+        }
 
         btnpreAct.setOnClickListener {
             intent = Intent(applicationContext, MainActivity::class.java)
